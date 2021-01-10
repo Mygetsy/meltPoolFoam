ee56b118a3e5fd57bf2876a49fec45e26c5f1ff4
Oleg Rogozin <o.a.rogozin@gmail.com>
2021-01-10 23:37:48 +0300

    [singleTrackPrinting] share plot_deltaT.sh with others

--- run/singleTrackPrinting/plot_deltaT.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-appl=$(grep application system/controlDict | egrep -o '\w+Foam')
-
-gnuplot -p -e "set format y '%.1e'; set log y; plot '<&3' w lp" \
-    3< <(grep deltaT log.$appl | awk '{print $3}')
